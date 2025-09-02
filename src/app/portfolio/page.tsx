a451a474d07c85f33be661c8c5252a9763711782
Jose Velarde <josevelarde@MacBookAir.attlocal.net>
2025-09-02 15:12:38 -0500

    fixing image issue x2

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -6,22 +6,22 @@ export default function PortfolioPage() {
       <h1 className="text-3xl font-bold mb-6">Portfolio</h1>
       <div className="grid grid-cols-2 gap-6">
         <Image
-          src="/images/mariel_kuncartcuadro1.jpg"
+          src="/images/mariel_kuncart_cuadro1.jpg"
           alt="Painting 1"
           width={600}
           height={400}
           className="rounded shadow"
         />
         <Image
-          src="/images/mariel_kuncartcuadro2.jpg"
+          src="/images/mariel_kuncart_cuadro2.jpg"
           alt="Painting 2"
           width={600}
           height={400}
           className="rounded shadow"
         />
         <Image
-          src="/images/mariel_kuncartcuadro3.jpg"
-          alt="Painting 2"
+          src="/images/mariel_kuncart_cuadro3.jpg"
+          alt="Painting 3"
           width={600}
           height={400}
           className="rounded shadow"
